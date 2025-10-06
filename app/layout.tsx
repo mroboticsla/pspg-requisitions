e966540727107a7934d4f98e46c2d7674b9c28cc
Mauricio Montoya <mauricio.montoya@mroboticsla.com>
2025-10-05 18:57:24 -0600

    feat: ajustar variables de color y aplicar clase de texto en el cuerpo para mejorar la accesibilidad

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 
   return (
     <html lang="es">
-      <body className={inter.className}>
+      <body className={`${inter.className} text-brand-dark`}>
         <AuthProvider>
           <div className="min-h-screen bg-surface-secondary">
             <Header />
